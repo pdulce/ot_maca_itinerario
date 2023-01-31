52990b27c7df3f07e96af3326aecd6498e7b7b14
99GU3997 <pedro.dulce@at.seg-social.es>
2023-01-31 08:54:11 +0100

    clean code

--- pipes/BUILD/CodeReview.groovy
+++ pipes/BUILD/CodeReview.groovy
@@ -2,7 +2,6 @@
 // devuelve 0 si OK  y <>0 si ERROR
 def start() {
 	log.info "REVISON CODIGO"
-	log.info "SCRIPT EVALUACION DE SONAR"
 	
 	return 0; // devolveremos como error la fase de compilacion para que no continue el pipe
 }
--- pipes/BUILD/UnitTest.groovy
+++ pipes/BUILD/UnitTest.groovy
@@ -2,7 +2,6 @@
 // devuelve 0 si OK  y <>0 si ERROR
 def start() {
 	log.info "PRUEBAS UNITARIAS"
-	log.info "SCRIPT EVALUACION DE SONAR"
 
 	junit allowEmptyResults: true, healthScaleFactor: 1.0, keepLongStdio: true, testResults: "**/build/test-results/test/*.xml"
 	
